bb0c151d02422adf896113a89ce23110dc70eccc
Tommy Bruzzese <tommybruzzese.tb@gmail.com>
2015-07-17 14:49:12 +0000

    almost final draft

---
+++ models/model.rb
@@ -0,0 +1,86 @@
+def puts_lyric_item
+  lyrics_array = ["“Emancipate yourselves from mental slavery.
+None but ourselves can free our minds.” ― Bob Marley", 
+    "“Don't criticize what you can't understand.” ― Bob Dylan", 
+    "“you're an expert at sorry and keeping the lines blurry” ― Taylor Swift", 
+    "“I get by with a little help from my friends.” ― John Lennon", 
+    "“When pain brings you down, don't be silly, don't close your eyes and cry, you just might be in the best position to see the sunshine.” ― Alanis Morissette", 
+    "“You have every right to a beautiful life.” ― Selena Gómez", 
+    "“Lose your dream, you lose your mind.” ― Rolling Stones", 
+    "“sleeping all these demons away” ― Ella Henderson ", 
+    "“I'll be the beauty queen in tears” ― Lorde", 
+    "“Don’t turn your back on me. I won’t be ignored” – Linkin Park", 
+    "“It's my party and I'll cry if I want to!” – Melanie Martinez", 
+    "“it's a new artform” ― Lorde", 
+    "“laughing 'til our ribs get tough” ― Lorde", 
+    "“I am not a white teeth teen.” ― Lorde", 
+    "“Glory and Gore go hand in hand” ― Lorde" , 
+    "“I'm lying in the ocean, singing your song.” ― Lana del Ray", 
+    "“and there is no remedy for memory” ― Lana del Rey", 
+    "“We're the wild ones. Curious fools. Trapped in the flume.” ― Baharia",
+    "“war inside my mind, drowning in the tides.” ― Baharia",
+    "“you're the magician's assitant in their dreams” ― Vance Joy",
+    "“running down to the riptide.” ― Vance Joy",
+    "“be my great escape 'til we fade away.” ― Baharia",
+    "“in silent screams. in wildest dreams. I never dreamed of this.” ― Taylor Swift",
+    "“raised by wolves, we howl to the moon.” ― Baharia",
+    "“it's like a dark paradise.” ― Lana del Rey",
+    "“now I need someone to breathe me back to life.” ― Shawn Mendes",
+    "“you watch me beat until I can't breathe.” ― Shawn Mendes",
+    "“got a feeling that I'm going under.” ― Shawn Mendes"]
+  lyrics_array[rand(lyrics_array.count)]
+end
+
+def puts_nature_item
+  nature_array = ["the best thing one can do when it's raining is to let it rain.", "Just living is not enough... one must have sunshine, freedom, and a little flower.", 
+    "there are always flowers for those who want to see them",
+    "some people walk in the rain, others just get wet",
+    "light in nature creates the movement of colors",
+    "Rain is grace; rain is the sky descending to the earth; without rain, there would be no life",
+    "stars can't shine without darkness",
+    "His hat was old, his coat worn, the water passed through his shoes and the stars through his soul.",
+    "Watch the stars, and see yourself running with them.",
+    "Magic exists. Who can doubt it, when there are rainbows and wildflowers, the music of the wind and the silence of the stars?",
+    "a rainbow fiesta.",
+    "in sunlight, starlight, and moonlight.", "running to the river", ""]
+  nature_array[rand(nature_array.count)]
+end
+
+def puts_love_item  
+  love_array = ["“It is better to be hated for what you are than to be loved for what you are not.” ― André Gide, Autumn Leaves",
+    "“You've gotta dance like there's nobody watching, Love like you'll never be hurt, Sing like there's nobody listening, And live like it's heaven on earth.” ― William W. Purkey", 
+    "“You know you're in love when you can't fall asleep because reality is finally better than your dreams.” ― Dr. Seuss", 
+    "“Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.” ― Martin Luther King Jr.", 
+    "“You don't love someone because they're perfect, you love them in spite of the fact that they're not.” ― Jodi Picoult, My Sister's Keeper",
+    "“Tis better to have loved and lost, than never to have loved at all.” ― Alfred Lord Tennyson, In Memoriam",
+    "“When I despair, I remember that all through history the way of truth and love have always won. There have been tyrants and murderers, and for a time, they can seem invincible, but in the end, they always fall. Think of it--always.” ― Mahatma Gandhi",
+    "“One is loved because one is loved. No reason is needed for loving.”― Paulo Coelho, The Alchemist",
+    "“Where there is love, there is life.” ― Mahatma Gandhi",
+    "“What's meant to be will always find a way” ― Trisha Yearwood",
+    "Anyone who has loved has been touched by magic. It is such a simple and such an extraordinary part of the lives we live."]  
+  love_array[rand(love_array.count)]
+end
+
+def puts_philosophy_item
+  philosophy_array = ["This is my simple religion. There is no need for temples; no need for complicated philosophy. Our own brain, our own heart is our temple; the philosophy is kindness.",
+    "I have a simple philosophy: Fill what's empty. Empty what's full. Scratch where it itches.",
+    "The philosophy of the school room in one generation will be the philosophy of government in the next.",
+    "You can be creative in anything - in math, science, engineering, philosophy - as much as you can in music or in painting or in dance." "Simplicity is the ultimate sophistication.",
+    "Live simply so that others may live simply.",
+    "Eventually, you might see the good in 'goodbye'",
+    "I can't go back to yesterday because I was a different person then.", 
+    "No matter how many plans you make or how much in control you are, life is always winging it.",
+    "A ship is always safe at the shore, but that is not what it is built for.",
+    "If you live it, you should love it!",
+    "We all have two lives, but the second starts when we realize we only have one.",
+    "The man on top of the mountain didn't fall there.", 
+    "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe. — Albert Einstein",
+    "Be the change that you wish to see in the world. — Mahatma Gandhi",
+    "Go to heaven for the climate and hell for the company. — Benjamin Franklin Wade",
+    "Have you ever noticed how ‘What the hell’ is always the right decision to make? — Terry Johnson (Insignificance)",
+    "Man is the only creature who refuses to be what he is. — Albert Camus",
+    "If a cluttered desk is a sign of a cluttered mind, of what, then, is an empty desk a sign? — Albert Einstein",
+    "Never let your sense of morals prevent you from doing what is right. 
+— Isaac Asimov (Foundation (Foundation, #1))" ]
+  philosophy_array[rand(philosophy_array.count)]
+end 
