bf40bd12fac65ca444c7f44039e804fb056653dc
Tommy Bruzzese <tommybruzzese.tb@gmail.com>
2015-07-20 23:22:33 +0000

    New added phrases update

--- models/model.rb
+++ models/model.rb
@@ -27,7 +27,32 @@ None but ourselves can free our minds.” ― Bob Marley",
     "“it's like a dark paradise.” ― Lana del Rey",
     "“now I need someone to breathe me back to life.” ― Shawn Mendes",
     "“you watch me beat until I can't breathe.” ― Shawn Mendes",
-    "“got a feeling that I'm going under.” ― Shawn Mendes"]
+    "“got a feeling that I'm going under.” ― Shawn Mendes",
+    "“Take me to wonderland ― Natalia Kills”",
+    "“we found wonderland.” ― Taylor Swift",
+    "“they say it's easy to leave you behind. I don't want to try.” ― Years & Years",
+    "“I was a king under your control.” ― Years & Years ",
+    "“I want to feel like you've let me go. So let me go” ― Years & Years",
+    "“Somewhere Only We Know” ― Keane",
+    "“I feel like I'm glued on tight to this Carousel” ― Melanie Martinez",
+    "“Chasing after you is like a fairytale” ― Melanie Martinez",
+    "“You sprinkled stardust on my pillowcase” ― Echosmith",
+    "“I'm in over my head.” ― FLETCHER",
+    "“like thunder needs the rain” ― FLETCHER",
+    "“Too late to leave these walls. This castle's been surrounded. Won't stay to watch it fall around us” ― FLETCHER",
+    "“Watch me put my war paint on, my war paint on” ― FLETCHER",
+    "“Now we're lost somewhere in outer space” ― Halsey",
+    "“Scream in the name of a foreigner's God ” ― Hozier",
+    "“We gladiate, but I guess we are really fighting ourselves” ― Lorde",
+    "“let your colors bleed & blend with mine” ― Of Monsters and Men",
+    "“Freshly disowned in a frozen devotion” ― Hozier",
+    "“Only then I am human. Only then I am clean.” ― Hozier",
+    "“I can't find you in the dark” ― Cathedrals",
+    "“When I run to the sea till sun shines” ― Cathedrals",
+    "“Let's get out of this town babe, we're on fire” ― Lana Del Rey",
+    "“but now's our curtain call” ― Kygo",
+    "“and I will haunt you like a ghost” ― Margot and the Nuclear So & So's",
+    "“and I wrote this on an airplane where people looked like ants.” ― Margot and the Nuclear So & So's"]
   lyrics_array[rand(lyrics_array.count)]
 end
 
@@ -44,7 +69,15 @@ def puts_nature_item
     "Magic exists. Who can doubt it, when there are rainbows and wildflowers, the music of the wind and the silence of the stars?",
     "in sunlight, starlight, and moonlight.",
     "running to the river", 
-    "reach for the sky, and you'll land on the moon."]
+    "reach for the sky, and you'll land on the moon.",
+    "Sunset is still my favorite color, and rainbow is second.",
+    "Behind every cloud is another cloud, and I love that.",
+    "Nature always wears the colors of the spirit.",
+    "Wherever you go, no matter what the weather, always bring your own sunshine.",
+    "No snowflake ever falls in the wrong place.",
+    "And as we wind on down the road...",
+    "when the fireworks soar above",
+    "typos are for kool kids"]
   nature_array[rand(nature_array.count)]
 end
 
@@ -60,7 +93,15 @@ def puts_love_item
     "“Where there is love, there is life.” ― Mahatma Gandhi",
     "“What's meant to be will always find a way” ― Trisha Yearwood",
     "Anyone who has loved has been touched by magic. It is such a simple and such an extraordinary part of the lives we live.",
-    "Within the work of hatred, we find the world of love."]  
+    "Within the work of hatred, we find the world of love.",
+    "fade me.",
+    "I was lovely once, but he never loved me once",
+    "I knew my feelings for you were real when I spent more time thinking about you than worrying about myself.",
+    "You are every reason, every hope and every dream I've ever had.",
+    "Love yourself first, because you are most important.",
+    "Be confident!",
+    "Follow love and it will flee, flee love and it will follow.",
+    "The rose speaks of love silently, in a language known only to the heart."]  
   love_array[rand(love_array.count)]
 end
 
@@ -92,6 +133,20 @@ def puts_philosophy_item
     "nothing is worse than realizing it was national ice-cream day after it's over.",
     "life's too short to not always try and be happy.",
     "never stop learning on the road to wisdom!",
-    "We are the leaders of the not coming back"]
+    "We are the leaders of the not coming back.",
+    "It is scientifically proven that one person's emoitons affet all those around them. Be happy and make everyone happy.",
+    "Even if you just try to smile, trying is better than giving up.",
+    "Some of the best advice I've ever gotten is that you'll still make it even if you fake it. So smile. Laugh. Try to just be happy, and eventually you won't have to even try anymore, because it will just be natural. :)", 
+    "Knock on the sky and listen to the sound.",
+    "Tomorrow always comes, and today is never yesterday.",
+    "You can see a lot by just looking.",
+    "The opposite of a correct statement is a false statement. But the opposite of a profound truth may well be another profound truth.",
+    "There are things I have wanted so long that I would only consent to have them if I could keep wanting them.",
+    "Eggs cannot be unscrambled!",
+    "I want everything to be perfect, but where the hell is the fun in that?",
+    "The dreams that are meant to, WILL come true.",
+    "Wish on every shooting star, on every birthday candle, and every 11:11.",
+    "it's simple, but: be you.",
+    "shirt inside out & life upside down"]
   philosophy_array[rand(philosophy_array.count)]
 end 
