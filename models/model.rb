54908bea332acc41fee2aad628903894162f873a
Tommy Bruzzese <tommybruzzese.tb@gmail.com>
2015-07-20 03:13:30 +0000

    redesign

--- models/model.rb
+++ models/model.rb
@@ -32,7 +32,8 @@ None but ourselves can free our minds.” ― Bob Marley",
 end
 
 def puts_nature_item
-  nature_array = ["the best thing one can do when it's raining is to let it rain.", "Just living is not enough... one must have sunshine, freedom, and a little flower.", 
+  nature_array = ["the best thing one can do when it's raining is to let it rain.",
+    "Just living is not enough... one must have sunshine, freedom, and a little flower.", 
     "there are always flowers for those who want to see them",
     "some people walk in the rain, others just get wet",
     "light in nature creates the movement of colors",
@@ -41,23 +42,25 @@ def puts_nature_item
     "His hat was old, his coat worn, the water passed through his shoes and the stars through his soul.",
     "Watch the stars, and see yourself running with them.",
     "Magic exists. Who can doubt it, when there are rainbows and wildflowers, the music of the wind and the silence of the stars?",
-    "a rainbow fiesta.",
-    "in sunlight, starlight, and moonlight.", "running to the river", ""]
+    "in sunlight, starlight, and moonlight.",
+    "running to the river", 
+    "reach for the sky, and you'll land on the moon."]
   nature_array[rand(nature_array.count)]
 end
 
 def puts_love_item  
-  love_array = ["“It is better to be hated for what you are than to be loved for what you are not.” ― André Gide, Autumn Leaves",
-    "“You've gotta dance like there's nobody watching, Love like you'll never be hurt, Sing like there's nobody listening, And live like it's heaven on earth.” ― William W. Purkey", 
+  love_array = ["“It's better to be hated for what you are than be loved for what you aren't.” ― André Gide,",
+    "“You've gotta dance like there's nobody watching, Love like you'll never be hurt, Sing like there's nobody listening, and live like it's heaven on earth.” ― William W. Purkey", 
     "“You know you're in love when you can't fall asleep because reality is finally better than your dreams.” ― Dr. Seuss", 
-    "“Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.” ― Martin Luther King Jr.", 
-    "“You don't love someone because they're perfect, you love them in spite of the fact that they're not.” ― Jodi Picoult, My Sister's Keeper",
-    "“Tis better to have loved and lost, than never to have loved at all.” ― Alfred Lord Tennyson, In Memoriam",
-    "“When I despair, I remember that all through history the way of truth and love have always won. There have been tyrants and murderers, and for a time, they can seem invincible, but in the end, they always fall. Think of it--always.” ― Mahatma Gandhi",
-    "“One is loved because one is loved. No reason is needed for loving.”― Paulo Coelho, The Alchemist",
+    "“Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.” ― MLK Jr.", 
+    "“You don't love someone because they're perfect, you love them in spite of the fact that they're not.” ― Jodi Picoult,",
+    "“It's better to have loved and lost, than never to have loved at all.” ― Alfred Lord Tennyson",
+    "“all through history, the way of truth and love has always won. There have been tyrants, but in the end, they always fall.” ― Mahatma Gandhi",
+    "“One is loved because one is loved. No reason is needed for loving.”― Paulo Coelho",
     "“Where there is love, there is life.” ― Mahatma Gandhi",
     "“What's meant to be will always find a way” ― Trisha Yearwood",
-    "Anyone who has loved has been touched by magic. It is such a simple and such an extraordinary part of the lives we live."]  
+    "Anyone who has loved has been touched by magic. It is such a simple and such an extraordinary part of the lives we live.",
+    "Within the work of hatred, we find the world of love."]  
   love_array[rand(love_array.count)]
 end
 
@@ -66,7 +69,7 @@ def puts_philosophy_item
     "I have a simple philosophy: Fill what's empty. Empty what's full. Scratch where it itches.",
     "The philosophy of the school room in one generation will be the philosophy of government in the next.",
     "You can be creative in anything - in math, science, engineering, philosophy - as much as you can in music or in painting or in dance." "Simplicity is the ultimate sophistication.",
-    "Live simply so that others may live simply.",
+    "Just live.",
     "Eventually, you might see the good in 'goodbye'",
     "I can't go back to yesterday because I was a different person then.", 
     "No matter how many plans you make or how much in control you are, life is always winging it.",
@@ -75,12 +78,16 @@ def puts_philosophy_item
     "We all have two lives, but the second starts when we realize we only have one.",
     "The man on top of the mountain didn't fall there.", 
     "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe. — Albert Einstein",
-    "Be the change that you wish to see in the world. — Mahatma Gandhi",
+    "Be the change that you wish to seek in yourself.",
     "Go to heaven for the climate and hell for the company. — Benjamin Franklin Wade",
     "Have you ever noticed how ‘What the hell’ is always the right decision to make? — Terry Johnson (Insignificance)",
     "Man is the only creature who refuses to be what he is. — Albert Camus",
     "If a cluttered desk is a sign of a cluttered mind, of what, then, is an empty desk a sign? — Albert Einstein",
-    "Never let your sense of morals prevent you from doing what is right. 
-— Isaac Asimov" ]
+    "Never let your sense of morals prevent you from doing what is right. — Isaac Asimov",
+    "Happiness is infectious & laughter is contagious",
+    "life is boundless.",
+    "find a passion and love it endlessly.",
+    "create yourself.",
+    "Be young, be dope, be proud -Lana Del Rey"]
   philosophy_array[rand(philosophy_array.count)]
 end 
